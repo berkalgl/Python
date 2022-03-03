0f1849c6ebf63ae593168e71f6f481dc224956e9
berkalgl <berkalgull@gmail.com>
2022-03-03 15:03:02 +0300

    Basic lists

--- Basic/python_201.py
+++ Basic/python_201.py
@@ -1,7 +1,44 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar  2 14:38:23 2022
+#DATA STRUCTURES
+#--------------------------------------
+#LISTS
 
-@author: TCBALGUL
-"""
+notes = [90,80,70,50, "a", [1,2,3,4]]
 
+type(notes[5])
+
+notes[0:4]
+notes[:4]
+notes[4:]
+
+#Change Value
+notes[1] = 60
+#Add value
+notes = notes + [100]
+#Delete Value
+#del notes[0]
+
+#LIST METHODS
+dir(list)
+#append
+notes.append(200)
+#remove
+notes.remove(200)
+#insert
+notes.insert(2, 20)
+#pop
+notes.pop(len(notes)-1)
+#count
+notes.count(200)
+#copy
+notes2 = notes.copy()
+#extend
+notes.extend(notes2)
+#index
+notes.index(90)
+#reverse
+notes.reverse()
+#sort
+notes.sort()
+#clear
+notes.clear()
+#--------------------------------------

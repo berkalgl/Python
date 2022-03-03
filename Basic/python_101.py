ce40caea2648d53c0a7e8e169a78facef6867bb6
TCBALGUL <berk.algul@turkcell.com.tr>
2022-03-03 13:50:00 +0300

    python academy 101

---
+++ Basic/python_101.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  1 15:18:41 2022
+
+@author: TCBALGUL
+"""
+
+a="Hello world"
+#del a
+#String methods 
+#len
+len(a);
+
+#toUpper and toLower
+a.lower()
+a.upper()
+a.islower()
+a.isupper()
+
+#replace
+a.replace('e', 'o')
+
+#strip to trim spaces
+a.strip()
+
+#dir to see the methods and attr
+dir(a)
+a.capitalize() #first word 
+a.title() #every word
+
+#substrings
+a[0:5]
+
+#Type transformations
+firstNumber = input()
+int(firstNumber)
+float(firstNumber)
+str(firstNumber)
+
+#print
+print("Hello", "World", sep="_")
+#?print
+"_Python_".strip("_")
+
+"10" + 2
+"Merhaba! ".strip("")
---
+++ Basic/python_201.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  2 14:38:23 2022
+
+@author: TCBALGUL
+"""
+

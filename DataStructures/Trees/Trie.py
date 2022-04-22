5ab08e91a92c6a5996fb65a65153ededd0c319f1
Berk Algül <berkalgull@gmail.com>
2022-04-23 02:28:45 +0300

    trie

---
+++ DataStructures/Trees/Trie.py
@@ -0,0 +1,18 @@
+# A trie is specialized tree used in searching most often text
+# in most cases, it can outperform binary search trees, hash tables and most other data structures
+# trie allows you to know a part of word exists in a text
+
+# with empty root point.
+
+#                          start
+#          a           d           n            z
+#      r       s      o          e   o            e
+#    e                t         w     t            n
+#                              s         
+# prefix tree
+# which proves to be quite efficient in solving these problems,spesific strings, auto completion.
+# searching words in a dictionary, search engines, ip addresses etc.
+# the benefit of this type of structure is speed and space
+
+# O(length of the word) time complexity
+# space complexity, with prefixs we have advantages on memory space.

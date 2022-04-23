c05b1de68ecc8526c3d61f6504480f2a3a599bdf
Berk Algül <berkalgull@gmail.com>
2022-04-24 02:04:45 +0300

    graphs initialized

---
+++ DataStructures/Graphs/GraphsMain.py
@@ -0,0 +1,21 @@
+# Graph is simply a set of values that are related in a pairwise fashion
+# each item called node (vortex), every connection is called an edge
+# networks, www, facebook social network, amazon recommodation, google short path for google maps
+
+# Linked list are type of trees, trees are type of graphs
+
+# they can be directed or undirected
+# directed are oneway street, undirected you can go and go back
+# adding friend on facebook is undirected
+# however on twitter or instagram, following a person is directed graph
+
+# weighted and unweighted graph
+# weighted graphs contains values in their edges as well
+# weighted graphs can help us to find the optimum paths 
+
+# Cyclic and Acyclic graph
+# you can go where you started in cyclic graph. every nodes has connection themselves
+
+# favorite graph : https://internet-map.net
+
+# https://visualgo.net/en/graphds?slide=1

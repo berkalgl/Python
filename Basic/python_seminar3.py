9b75d19941e9f71b9d1adc51b856b62dcd333848
berkalgl <berkalgull@gmail.com>
2022-04-28 12:32:58 +0300

    seminar3 finalized

--- Basic/python_seminar3.py
+++ Basic/python_seminar3.py
@@ -182,6 +182,20 @@ agg_list = ['mean', 'min', 'max', 'var']
 ####
 # purpose: make the first column key and array of rest of the row.
 ####
+# # before
+#    total  speeding  alcohol  not_distracted  no_previous
+# 0   18.8     7.332    5.640          18.048       15.040
+# 1   18.1     7.421    4.525          16.290       17.014
+# 2   18.6     6.510    5.208          15.624       17.856
+# 3   22.4     4.032    5.824          21.056       21.280
+# 4   12.0     4.200    3.360          10.920       10.680
+
+# after:
+# {18.8: [7, 5, 18, 15],
+#  18.1: [7, 4, 16, 17],
+#  18.6: [6, 5, 15, 17],
+#  22.4: [4, 5, 21, 21],
+#  12.0: [4, 3, 10, 10]}
 
 
 df = sns.load_dataset("car_crashes")

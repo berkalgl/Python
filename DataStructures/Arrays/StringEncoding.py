a28c0839424e500a09c1c92636769bacaa42276e
Berk Algül <berkalgull@gmail.com>
2022-05-09 23:54:46 +0300

    StringChallenge add

---
+++ DataStructures/Arrays/StringEncoding.py
@@ -0,0 +1,16 @@
+def StringChallenge(sentence):
+    n = len(sentence)
+    result = ""
+    i = 0
+    while i<n-1:
+        count = 1
+        while i < n-1 and sentence[i] == sentence[i+1]:
+            count += 1
+            i += 1
+        result = result + str(count) + sentence[i]
+        i+=1
+
+
+    return result
+
+print(StringChallenge("aaaabbbccd"))

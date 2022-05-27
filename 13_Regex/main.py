fee1489d1fd94c897789b37ab29956d9a2dcf40f
Berk Algül <berkalgull@gmail.com>
2022-05-27 22:22:38 +0300

    Regex 2

--- 13_Regex/main.py
+++ 13_Regex/main.py
@@ -1,4 +1,5 @@
 # https://www.w3schools.com/python/python_regex.asp
+# https://regex101.com/
 import re
 
 string = 'search inside of this text please!'
@@ -17,3 +18,6 @@ b = pattern.search(string)
 c = pattern.findall(string)
 d = pattern.fullmatch(string)
 e = pattern.match(string)
+
+pattern2 = re.compile(r"([a-zA-Z]).([a])") # r stands for raw string / pure string
+string = 'search this inside of this text please ! Berk'
